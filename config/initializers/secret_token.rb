de2cf2a18051459cd367af46671f239df5e1d6fc
raphael <raphael@raphael-Capella-IbexPeak-M-Chipset.(none)>
2012-10-09 09:46:19 -0300

    Primeiro Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logistica::Application.config.secret_token = '01b050890ceb6127e3ca88f9b54230efaa7eb1580c3540d37222218f35e3f50e311a0c44dbc5107d5d5723789b27de7e44e8546e01a5abaef15db40365e41d99'
